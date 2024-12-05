1bfc1ae9468b67c3e2f4143a632b1dd6dafde144
Patrick Carver <patrickcarver@gmail.com>
2024-12-04 20:31:10 -0600

    Day 4 Part 2 complete

--- day04/lib/day04.ex
+++ day04/lib/day04.ex
@@ -18,26 +18,37 @@ defmodule Day04 do
     [{0, 0}, {1, 1}, {2, 2}, {3, 3}]
   ]
 
-  @xmas ~w[X M A S]
+  @xmas [{-1, -1}, {1, 1}, {-1, 1}, {1, -1}]
 
   def part1(file_name \\ "input.txt") do
     ("priv/" <> file_name)
+    |> load_grid()
+    |> count_xmas()
+  end
+
+  def part2(file_name \\ "input.txt") do
+    ("priv/" <> file_name)
+    |> load_grid()
+    |> count_x_mas()
+  end
+
+  defp load_grid(file_name) do
+    file_name
     |> File.stream!()
     |> Stream.map(&String.trim_trailing/1)
     |> build_grid()
-    |> count_xmas()
   end
 
   defp count_xmas(grid) do
     coords = Map.keys(grid)
 
-    Enum.reduce(coords, 0, fn {x, y} = coord, acc ->
+    Enum.reduce(coords, 0, fn {x, y}, acc ->
       count =
         Enum.reduce(@directions, 0, fn direction, dir_acc ->
           candidate =
             Enum.map(direction, fn {dir_x, dir_y} -> Map.get(grid, {x + dir_x, y + dir_y}) end)
 
-          if candidate == @xmas do
+          if candidate == ~w[X M A S] do
             dir_acc + 1
           else
             dir_acc
@@ -45,6 +56,21 @@ defmodule Day04 do
         end)
 
       acc + count
+    end)
+  end
+
+  defp count_x_mas(grid) do
+    letter_as = grid |> Map.filter(fn {_, letter} -> letter == "A" end) |> Map.keys()
+
+    Enum.reduce(letter_as, 0, fn {x, y}, acc ->
+      candidate =
+        Enum.map(@xmas, fn {dir_x, dir_y} -> Map.get(grid, {x + dir_x, y + dir_y}) end)
+
+      if candidate in [~w[M S M S], ~w[M S S M], ~w[S M S M], ~w[S M M S]] do
+        acc + 1
+      else
+        acc
+      end
     end)
   end
 
